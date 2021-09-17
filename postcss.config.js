cca1f0e4ee5de5dee3229db27a2a452365c667df
Eric Mertens <emertens@gmail.com>
2021-09-16 22:15:22 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -6,5 +6,14 @@ module.exports = {
     require('tailwindcss')('./_includes/tailwind.config.js'),
     require('postcss-preset-env'),
     require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['!(_site|node_modules)/**/*.+(html|js|md)', '*.html'],
+      safelist: [/::(before|after)/],
+      defaultExtractor: (content) =>
+        content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+    require('cssnano')({
+      preset: 'default',
+    }),
   ]
 }
